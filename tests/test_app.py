e72f0c50a504353b4555fe5bca07aeecd8e58141
NatalNW7 <natanaelweslley1@gmail.com>
2024-09-07 15:56:21 -0300

    feat: initial commit

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from todo_list.app import app
+
+
+def test_index():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Hello, World!'}
