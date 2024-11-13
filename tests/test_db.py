996cdd93fbd64d70eed3c8541e7e93a426a86eb0
NatalNW7 <natanaelweslley1@gmail.com>
2024-11-12 22:25:33 -0300

    feat: create model `User`

---
+++ tests/test_db.py
@@ -0,0 +1,24 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
+from todo_list.models import User
+
+
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='jonas', password='jonas123', email='clonacartao@suporte'
+        )
+        session.add(new_user)
+        session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'jonas'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': new_user.username,
+        'password': new_user.password,
+        'email': new_user.email,
+        'created_at': time,
+    }
